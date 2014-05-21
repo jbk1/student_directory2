f156c2fc9c1d2d8d6aee27af34668c523beda6a5
James B Kemp <james@bibble.com>
2014-05-21 16:51:52 +0100

    created students,list & put in list

--- lib/st_directory.rb
+++ lib/st_directory.rb
@@ -1,3 +1,16 @@
-def new_student(name, cohort, year)
-	return {cohort: cohort, year: year, name: name}
-end     
+def create_student(name, cohort, year)
+	return {name: name, cohort: cohort, year: year}
+end
+
+
+def student_list
+	@list ||= []
+end
+
+def add_student(student)
+	student_list << student
+end
+
+# def new_student(name, cohort, year)
+# 	return {cohort: cohort, year: year, name: name}
+# end     
--- spec/st_directory_spec.rb
+++ spec/st_directory_spec.rb
@@ -1,28 +1,75 @@
-require 'st_directory.rb'
+#  START WITH LOAD DATA
+require 'st_directory'
+
+describe "Student directory" do
+
+  context "creating student" do
+    it "is an object with 3 attributes" do
+      expect(create_student("Mihai", "May", 2014)).to eq ({name: "Mihai", cohort: "May", year: 2014})
+    end
+  end
+
+  context "creating a list of students" do
+    it "the list is empty before it's populated" do
+      # expect(student_list).to eq []
+      expect(student_list).to be_empty
+    end
+
+    it "puts a student into the list" do
+      # mihai = create_student("Mihai", "May", 2014)
+      james = create_student("James", "May", 2014)
+      # add_student(mihai)
+      add_student(james)
+
+      expect(student_list).to eq [james]
+      # expect(student_list.include?(mihai)).to be_true
+      # expect(student_list.include?(james)).to be_true
+    end
+  end
 
 
-describe "creating students" do
 
-  it "with name, cohort and year" do
-  	student = {name: 'John', cohort: :may, year: 2014}
-  	expect(new_student('John', :may, 2014)).to eq student 
-	end
-
-  it "with a different name, cohort and year" do
-  	student = {name: 'Dave', cohort: :may, year: 2014}
-  	expect(new_student('Dave', :may, 2014)).to eq student
-	end
-
-  it "with a name, different cohort and year" do
-  	student = {name: 'John', cohort: :april, year: 2014}
-  	expect(new_student('John', :april, 2014)).to eq student
-	end
-
-  it "with a name, cohort and different year" do
-  	student = {name: 'John', cohort: :april, year: 2013}
-  	expect(new_student('John', :april, 2013)).to eq student
-	end
 end
 
-describe "creating a student directory" do
-	
+
+
+
+# context "display students"
+
+# context "editing student has_a_file"
+
+
+
+
+
+
+
+
+# require 'st_directory.rb'
+
+
+# describe "creating students" do
+
+#   it "with name, cohort and year" do
+#   	student = {name: 'John', cohort: :may, year: 2014}
+#   	expect(new_student('John', :may, 2014)).to eq student 
+# 	end
+
+#   it "with a different name, cohort and year" do
+#   	student = {name: 'Dave', cohort: :may, year: 2014}
+#   	expect(new_student('Dave', :may, 2014)).to eq student
+# 	end
+
+#   it "with a name, different cohort and year" do
+#   	student = {name: 'John', cohort: :april, year: 2014}
+#   	expect(new_student('John', :april, 2014)).to eq student
+# 	end
+
+#   it "with a name, cohort and different year" do
+#   	student = {name: 'John', cohort: :april, year: 2013}
+#   	expect(new_student('John', :april, 2013)).to eq student
+# 	end
+# end
+
+# describe "creating a student directory" do
+# 	
