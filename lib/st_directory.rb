4a16b44f50e2bdffff936fb9c506ffbee9e95474
James B Kemp <james@bibble.com>
2014-05-22 18:13:49 +0100

    editing students finished

--- lib/st_directory.rb
+++ lib/st_directory.rb
@@ -1,24 +1,41 @@
-def create_student(name, cohort, year)
-	return {name: name, cohort: cohort, year: year}
-end
+	def create_student(name, cohort, year)
+		return {name: name, cohort: cohort, year: year}
+	end
 
-def student_list
-	@list ||= []
-end
+	def student_list
+		@list ||= []
+	end
 
-def add_student(student)
-	student_list << student
-end
+	def add_student(student)
+		student_list << student
+	end
 
-# Problem here whilst trying to genericise the method?
-def replace_a_hash_value_for(student, hash_key)
-	student[] = hash_key
-end
+# # Problem here whilst trying to genericise the method?
 # surely we should de-limit this from just year?
-def replaces_attribute_with_NA_for(student, hash_key)
-	empty = "N/A"
-	student[hash_key] = empty
-end
 
+	def replaces_attribute_with_NA_for(student, hash_key)
+		empty = "N/A"
+		student[hash_key] = empty
+	end
 
+	# def replace_student_year(student, new_year)
+	# 	student[:year] = new_year
+	# end
 
+	# def replace_student_month(student, new_month)
+	# 	student[:month] = new_month
+	# end
+
+	# def replace_student_name(student, new_name)
+	# 	student[:name] = new_name
+	# end
+
+# a refactored method of the above three. Tested by the tests within
+# the "Editing Students" section of _spec.
+	def replace_student_value(student, key_id, new_value)
+		student[key_id] = new_value
+	end
+
+	def input_student(name, cohort, year)
+		puts "Please enter student"
+	end
--- spec/st_directory_spec.rb
+++ spec/st_directory_spec.rb
@@ -8,50 +8,103 @@ describe "Student directory" do
       expect(create_student("Mihai", "May", 2014)).to eq ({name: "Mihai", cohort: "May", year: 2014})
     end
   end
-
+# ------------ -----
   context "creating a list of students" do
+    
     it "the list is empty before it's populated" do
-      # expect(student_list).to eq []
       expect(student_list).to be_empty
     end
 
     it "puts a student into the list" do
-      # mihai = create_student("Mihai", "May", 2014)
       james = create_student("James", "May", 2014)
-      # add_student(mihai)
       add_student(james)
-
       expect(student_list).to eq [james]
-      # expect(student_list.include?(mihai)).to be_true
-      # expect(student_list.include?(james)).to be_true
+    end
+  end
+# ------------ -----
+  context "Editing Students" do
+
+    it "replaces year with a new year" do
+      james = create_student("James", "May", 2014)
+      replace_student_value(james, :year, 2013)   
+      expect(james[:year]).to eq 2013
+    end
+
+      it "replaces month with a new month" do
+      james = create_student("James", "May", 2014)
+      replace_student_value(james, :month, "June")
+      expect(james[:month]).to eq "June" 
+    end
+
+      it "replaces name with a new name" do
+      james = create_student("James", "May", 2014)
+      replace_student_value(james, :name, "Dave")
+      expect(james[:name]).to eq "Dave"
     end
   end
 
-  context "editing the student list" do
+# ----------- -----
+  context "Inputting data" do
 
-    it "replaces student attributes" do
-      james = create_student("James", "May", 2014)
-      replace_a_hash_value_for(james, "April")
-      expect(james[]).to eq "April"      
+    # it "allows a user to create a student" do
+    # student = input_student(name, month, year)
+    # expect(student).to eq "{name: name, cohort: cohort, year: year}"
+    # end
+
+    it "allows a user to enter a student name" do
+      # student_name = input_student_name(name)
+      # expect(student_name).to eq 
+    end
+  end
+# ------------ -----
+  context "Writing to and loading from file" do
+
+
+    context "Writing to a file" do
+    
+      it "checks a csv file exists"
+
+      # looks in a directory, .find
+
+
+      it "saves data to a file"
+
     end
 
-    it "replaces any student attribute with N/A" do
-      james = create_student("James", "May", 2014)
-      replaces_attribute_with_NA_for(james, :year)
-      expect(james[:year]).to eq "N/A"
-    end
+    context "Loading to a file" do
+
+      it "loads a file"
 
 
-
-
+    end  
+  
   end
 
 
 
-
-
-
+# ------------ -----
 end
+
+  # context "editing the student list" do
+  #   it "replaces student attributes" do
+  #     # 1. Create two example students
+  #     # 2. Check that 
+
+
+    #   james = create_student("James", "May", 2014)
+    #   end
+    # end     
+
+    #   replace_cohort_value("May", "April")
+    #   expect(james[:cohort]).to eq "April"      
+    # end
+
+    # it "replaces any student attribute with N/A" do
+    #   james = create_student("James", "May", 2014)
+    #   replaces_attribute_with_NA_for(james, :year)
+    #   expect(james[:year]).to eq "N/A"
+    # end
+
 
 
 
